eebd6243300cd7e7dfdde6748caf55be570130da
Rohit <rohit.sharma2@hexaviewtech.com>
2023-10-02 12:14:05 +0530

    base-setup of project

---
+++ src/test/java/com/example/BloggerApp/BloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BloggerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
