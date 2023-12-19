0fd7937bc63f304be2c0e66063260ab89d0ed6a0
Rohit <rohit.sharma2@hexaviewtech.com>
2023-12-19 23:01:46 +0530

    create comment repository

---
+++ src/main/java/com/example/BloggerApp/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.example.BloggerApp.repository;
+
+import com.example.BloggerApp.models.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+
+}
