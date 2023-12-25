2e9d1d178751fbba1977031130515800223de190
Rohit <rohit.sharma2@hexaviewtech.com>
2023-12-25 23:20:22 +0530

    define bean of password encoder

--- src/main/java/com/example/BloggerApp/configurations/AppConfiguration.java
+++ src/main/java/com/example/BloggerApp/configurations/AppConfiguration.java
@@ -3,6 +3,8 @@ package com.example.BloggerApp.configurations;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfiguration {
@@ -10,5 +12,10 @@ public class AppConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
