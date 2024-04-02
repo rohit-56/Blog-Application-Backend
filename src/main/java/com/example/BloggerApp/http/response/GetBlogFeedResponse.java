9500668856a9e2b153a1e641f2ca492802d312aa
Rohit <rohit.sharma2@hexaviewtech.com>
2024-04-02 23:20:09 +0530

    create blog response class with user and category details

---
+++ src/main/java/com/example/BloggerApp/http/response/GetBlogFeedResponse.java
@@ -0,0 +1,51 @@
+package com.example.BloggerApp.http.response;
+
+import com.example.BloggerApp.models.Comment;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetBlogFeedResponse {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("subtitle")
+    private String subtitle;
+
+    @JsonProperty("getTagResponses")
+    private List<GetTagResponse> getTagResponses;
+
+    @JsonProperty("content")
+    private String content;
+
+    @JsonProperty("createdAt")
+    private String createdAt;
+
+    @JsonProperty("cover")
+    private String imageCover;
+
+    @JsonProperty("category")
+    private String category;
+
+    @JsonProperty("authorName")
+    private String authorName;
+
+    @JsonProperty("authorAvatar")
+    private String authorAvatar;
+
+    @JsonProperty("comments")
+    public Set<Comment> comments = new HashSet<>();
+
+}
