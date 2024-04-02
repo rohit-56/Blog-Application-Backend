b87b1ed6df5c9825a575384c42727ab4bde0c50b
Rohit <rohit.sharma2@hexaviewtech.com>
2024-04-02 23:21:03 +0530

    implement controller and service method for blog entity with user and category details

--- src/main/java/com/example/BloggerApp/repository/BlogRepository.java
+++ src/main/java/com/example/BloggerApp/repository/BlogRepository.java
@@ -3,6 +3,8 @@ package com.example.BloggerApp.repository;
 import com.example.BloggerApp.models.BlogEntity;
 import com.example.BloggerApp.models.CategoryEntity;
 import com.example.BloggerApp.models.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface BlogRepository extends JpaRepository<BlogEntity,Long> {
@@ -26,4 +29,8 @@ public interface BlogRepository extends JpaRepository<BlogEntity,Long> {
     List<BlogEntity> getBlogByCategoryEntity(CategoryEntity categoryEntity);
 
     List<BlogEntity> getBlogByUserEntity(UserEntity userEntity);
+
+    @Query(value = "SELECT mywork.blog_entity.id,mywork.blog_entity.title, mywork.category_entity.title as category,mywork.blog_entity.body,mywork.blog_entity.created_at,mywork.blog_entity.image_cover as cover,mywork.user_entity.image,mywork.user_entity.username from mywork.blog_entity inner join mywork.user_entity on mywork.blog_entity.user_entity_id=mywork.user_entity.id inner join mywork.category_entity on mywork.blog_entity.category_entity_id=mywork.category_entity.id ORDER BY mywork.blog_entity.id limit ?1 offset ?2 ;",
+           countQuery = "select count(*) from mywork.blog_entity;",nativeQuery = true)
+    List<Map<String,Object>> findAllBlogEntitywithUserDetails(long limit,long offset);
 }
